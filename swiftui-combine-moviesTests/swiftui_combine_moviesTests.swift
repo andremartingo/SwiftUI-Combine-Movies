3a23ea935677f85945818a0b4931dcc3abcbf0da
Andre Martingo <andre.martingo@gmail.com>
2019-06-06 15:21:25 +0200

    Initial Commit

---
+++ swiftui-combine-moviesTests/swiftui_combine_moviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_combine_moviesTests.swift
+//  swiftui-combine-moviesTests
+//
+//  Created by Andre Martingo on 06.06.19.
+//  Copyright © 2019 Andre Martingo. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_combine_movies
+
+class swiftui_combine_moviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
